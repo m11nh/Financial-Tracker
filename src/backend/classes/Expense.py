e1578c9c37e97b45541337892c907ee70e2f8326
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2020-08-30 17:52:54 +1000

    added code for log and expenses

---
+++ src/backend/classes/Expense.py
@@ -0,0 +1,8 @@
+class Expense(Log): 
+    def __init__(self, day: int, month: int, year: int, amount: float, notes: str,isNecessary : bool): 
+        super().__init__(day, month, year, amount, notes)
+        self._isNecessary = isNecessary 
+    
+    def getIsNecessary(self) -> bool: 
+        return self._isNecessary
+    
